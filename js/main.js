36c2afbd9309dd0e59f4cd807f5ff00c258a95f9
stellaris9950 <andyzihengwei@gmail.com>
2022-12-01 12:35:58 -0700

    doge

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@
 let signInBtn = document.getElementById('sign-in-btn');
 let signUpBtn = document.getElementById('sign-up-btn');
 
+
+// Array Storing Username & password
+let info = localStorage();
+
+
+
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener('click', signUpHandler);
 
@@ -15,5 +21,36 @@ function signUpHandler() {
 signInBtn.addEventListener('click', signInHandler);
 
 function signInHandler() {
-  console.log('Sign In Btn Clicked');
+    let newUser
+    let newPassord
+    info.push(newUser(newUsername, newPassord));
+    saveInfo();
+}
+
+
+
+
+
+
+// helper functions
+
+// new user
+function newUser(newUsername, newPassord){
+  return {
+    username: newUsername,
+    password: newPassord
+  };
+}
+
+// save global Contact to local storage
+function saveInfo() {
+  localStorage.setItem('Info', JSON.stringify(info))
+}
+
+
+// load Contact from local storage
+
+function loadStorage() {
+  let infoStr = localStorage.getItem('info');
+  return JSON.parse(infoStr) ?? [];
 }
